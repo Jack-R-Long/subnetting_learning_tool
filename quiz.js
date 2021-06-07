7f6657db21ebc9b479f00811704e264e04610d4f
Jack-R-Long <jack.richard.long@gmail.com>
2021-06-06 20:14:09 -0500

    Added pagination to quiz

--- quiz.js
+++ quiz.js
@@ -1,51 +1,4 @@
-// Select HTML elements
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-// object literals to hold questiosns
-const myQuestions = [
-    {
-        question: "What class is this IP address?",
-        ipAddress: "188.26.221.100",
-        answers: {
-            a: "Class A",
-            b: "Class B",
-            c: "Class C"
-        },
-        correctAnswer : "b"
-    },
-    {
-        question: "What class is this IP address?",
-        ipAddress: "128.125.132.191",
-        answers: {
-            a: "Class A",
-            b: "Class B",
-            c: "Class C"
-        },
-        correctAnswer : "b"
-    },
-    {
-        question: "What class is this IP address?",
-        ipAddress: "83.93.100.48",
-        answers: {
-            a: "Class A",
-            b: "Class B",
-            c: "Class C"
-        },
-        correctAnswer : "a"
-    },
-    {
-        question: "What class is this IP address?",
-        ipAddress: "204.158.32.45 ",
-        answers: {
-            a: "Class A",
-            b: "Class B",
-            c: "Class C"
-        },
-        correctAnswer : "c"
-    },
-]
+// Functions
 
 function buildQuiz(){
     // variable to store HTML output
@@ -79,9 +32,11 @@ function buildQuiz(){
 
             // add this question and its answer to the output
             output.push(
-                `<h3> IP address: ${currentQuestion.ipAddress}</h3>
-                <p> ${currentQuestion.question} </p>
-                <div class="answers"> ${answers.join('')} </div>`
+                `<div class="slide">
+                    <h3>${currentQuestion.ipAddress}</h3>
+                    <p> ${currentQuestion.question} </p>
+                    <div class="answers"> ${answers.join('')} </div>
+                </div>`
             );
 
         }
@@ -126,8 +81,95 @@ function showResults(){
   resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 }
 
-// display quiz right away
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+}
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Variables 
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const myQuestions = [
+    {
+        question: "What class is this IP address?",
+        ipAddress: "188.26.221.100",
+        answers: {
+            a: "Class A",
+            b: "Class B",
+            c: "Class C"
+        },
+        correctAnswer : "b"
+    },
+    {
+        question: "What class is this IP address?",
+        ipAddress: "128.125.132.191",
+        answers: {
+            a: "Class A",
+            b: "Class B",
+            c: "Class C"
+        },
+        correctAnswer : "b"
+    },
+    {
+        question: "What class is this IP address?",
+        ipAddress: "83.93.100.48",
+        answers: {
+            a: "Class A",
+            b: "Class B",
+            c: "Class C"
+        },
+        correctAnswer : "a"
+    },
+    {
+        question: "What class is this IP address?",
+        ipAddress: "204.158.32.45 ",
+        answers: {
+            a: "Class A",
+            b: "Class B",
+            c: "Class C"
+        },
+        correctAnswer : "c"
+    },
+]
+
+
+// Kick things off
 buildQuiz();
 
-// on submit, show results 
+// Pagination
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+// Show first slide
+showSlide(currentSlide);
+
+// Event listeners
 submitButton.addEventListener('click', showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
